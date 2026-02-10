c1021543f88907b01df0eded4463d5bc472aadcb
Alex Parra <9929parra@gmail.com>
2026-02-10 20:58:15 -0300

    Creación de los urls.py y la referencia a los mismos desde el config/urls.py

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.cash',
+    'apps.catalog',
+    'apps.core',
+    'apps.inventory',
+    'apps.sales',
+    'apps.users',
+
 ]
 
 MIDDLEWARE = [
