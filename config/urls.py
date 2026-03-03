a7302cf6cfba3418a99e3dc3e3d6e6e66a3cfaaa
Alex Parra <alex.parra@gonzalezgimenez.com>
2026-03-03 13:24:15 -0300

    Cambios en la estructura del sistema de la heladeria

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/cash/', include('apps.cash.urls')),
-    path('api/catalog/', include('apps.catalog.urls')),
-    path('api/core/', include('apps.core.urls')),
-    path('api/inventory/', include('apps.inventory.urls')),
-    path('api/sales/', include('apps.sales.urls')),
-    path('api/users/', include('apps.users.urls')),
 
 ]
